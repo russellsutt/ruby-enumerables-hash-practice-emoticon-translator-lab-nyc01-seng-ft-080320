9f829d5d814eefc4acf9b9726ba0a99b15edcdcc
Russell Sutter <russell.sutter@gmail.com>
2020-07-15 18:57:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,8 @@ require 'yaml'
 
 def load_library(file_path)
   yamfile = YAML.load_file(file_path)
-  
   new_library = {}
+  
   yamfile.each do |key, (value1, value2)|
     new_library[key]
   end
