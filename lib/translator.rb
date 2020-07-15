9f96f9e378d491a4c06ed1ba1d4dac5e88e8cbbd
Russell Sutter <russell.sutter@gmail.com>
2020-07-15 19:23:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   
   yamfile.each_with_object({}) do |(key, emoticons), new_hash|
     if !new_hash.include?(key)
-      new_hash[key] = []
+      new_hash[key] = {}
       binding.pry
     end
     emoticons.each do |emoticon|
