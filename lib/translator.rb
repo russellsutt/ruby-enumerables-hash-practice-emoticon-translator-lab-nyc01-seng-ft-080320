9d9d9198867340da34c1730a25b55e1dcd0187fd
Russell Sutter <russell.sutter@gmail.com>
2020-07-15 20:10:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,8 +23,8 @@ def get_english_meaning(file_path, jemoticon)
         english_meaning = meaning
       end
    end
-   return english_meaning
   end
+  return english_meaning
 end
 
 def get_japanese_emoticon(file_path)
