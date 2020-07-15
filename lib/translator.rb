b7b1636b9303e2e310ba23389f49a39088f42b3d
Russell Sutter <russell.sutter@gmail.com>
2020-07-15 20:31:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -31,6 +31,14 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
+  japanese_emoticon = ""
+  
+  libriary.each do |meaning, language|
+    if emoticon == library[meaning][:english]
+      japanese_emoticon = library[meaning][:japanese]
+    end
+  end
+  return japanese_emoticon
 end
 
 
