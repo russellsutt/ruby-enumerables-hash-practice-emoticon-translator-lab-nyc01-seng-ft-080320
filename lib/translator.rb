2e790b52a37d57344ffbdb8ea4c952af81ea575a
Russell Sutter <russell.sutter@gmail.com>
2020-07-15 20:09:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,8 +20,7 @@ def get_english_meaning(file_path, jemoticon)
   library.each do |meaning, language|
     if jemoticon == library[meaning][:japanese]
       english_meaning = meaning
-      end
-   end
+    end
   end
 end
 
