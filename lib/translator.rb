05b6fd871f1b4849f1c0036c50b2a10c0e81b169
Russell Sutter <russell.sutter@gmail.com>
2020-07-15 19:32:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(file_path)
     if !new_hash.include?(key)
       new_hash[key] = {}
     end
-    new_hash[key][:english]
+    new_hash[key][:english] = emoticons.first
     emoticons.each do |emoticon|
       
   end
