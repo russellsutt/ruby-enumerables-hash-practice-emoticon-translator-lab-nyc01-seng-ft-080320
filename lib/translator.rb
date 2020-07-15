3909ce885e75ec37f765b63e598ddd88763e7781
Russell Sutter <russell.sutter@gmail.com>
2020-07-15 19:54:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_english_meaning(file_path, jemoticon)
   
   library.each do |meaning, language|
     language.each do |emoticon|
-      if emoticon == jemoticon
+      if emoticon = jemoticon
         return library[meaning]
       end
    end
